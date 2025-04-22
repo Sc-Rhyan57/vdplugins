281dbbb86d75edf911bd7a2e8372fd7136395a33
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-04-22 15:17:54 -0300

    Update Settings.tsx

--- plugins/UserAV/src/Settings.tsx
+++ plugins/UserAV/src/Settings.tsx
@@ -14,28 +14,28 @@ export default () => (<ScrollView>
             label="Discord Server"
             leading={<FormRow.Icon source={getAssetIDByName("Discord")} />}
             trailing={FormRow.Arrow}
-            onPress={() => url.openDeeplink("https://discord.gg/SeuServidor")}
+            onPress={() => url.openDeeplink("https://dsc.gg/betterstar")}
         />
         <FormRow
             label="Recarregar API"
             leading={<FormRow.Icon source={getAssetIDByName("ic_message_retry")} />}
             onPress={async () => {
                 const fetch = await fetchData()
-                if (!fetch) return showToast("Falha ao recarregar a API de avatares", getAssetIDByName("small"))
-                return showToast("API de avatares recarregada!", getAssetIDByName("check"))
+                if (!fetch) return showToast("Falha ao recarregar api", getAssetIDByName("small"))
+                return showToast("BetterAvatars recarregada!", getAssetIDByName("check"))
             }}
         />
         <FormRow
             label="Como usar"
             leading={<FormRow.Icon source={getAssetIDByName("ic_help")} />}
             trailing={FormRow.Arrow}
-            onPress={() => url.openDeeplink("https://github.com/Seu-Usuario/UserAvatar")}
+            onPress={() => url.openDeeplink("https://dsc.gg/betterstar")}
             subLabel="Saiba como adicionar sua própria foto de perfil personalizada"
         />
         <FormRow
             label="API de avatares"
             leading={<FormRow.Icon source={getAssetIDByName("ic_link")} />}
-            subLabel="https://useravatar.vercel.app/v1"
+            subLabel="https://betterAvatars.vercel.app/v1"
         />
     </FormSection>
 </ScrollView>)
